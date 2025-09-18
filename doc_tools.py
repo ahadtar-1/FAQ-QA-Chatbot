0a7d7fca2f7009d89ec81b1cf3fde24fdae3b8bc
yzru01XesjWQwvwzSuOqNel9BCs23r0uNjR2 <yzru01XesjWQwvwzSuOqNel9BCs23r0uNjR2>
2025-09-18 20:51:11 +0500

    Document Parser added

--- doc_tools.py
+++ doc_tools.py
@@ -3,8 +3,49 @@ The module comprises of the tools to upload and parse PDFs.
 """
 
 import os
+import json
+import requests
 import shutil
 import gradio as gr
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+upstage_api_key = os.getenv("UPSTAGE_API_KEY")
+
+
+def parse_doc(file_path: str):
+    """
+    Sends a request to Upstage AI api to parse the PDF
+
+    Parameters
+    ----------
+    file_path : str
+        The file path of the PDF
+
+    Returns
+    -------
+    bool
+        The state of the response from the api      
+    
+    
+    
+    """
+
+
+    url = "https://api.upstage.ai/v1/document-digitization"
+    headers = {"Authorization": f"Bearer {upstage_api_key}"}
+    files = {"document": open(file_path, "rb")}
+    data = {"ocr": "force", "base64_encoding": "['table']", "model": "document-parse", "output_formats": "['html', 'text']"}
+    response = requests.post(url, headers=headers, files=files, data=data)
+    print(response.status_code)
+    if(response.status_code == 200):       
+        json_response = response.json()
+        name_of_file = os.path.basename(file_path)
+        with open(f"./{name_of_file}.json", "w", encoding="utf-8") as f:
+           json.dump(json_response, f, indent=4, ensure_ascii=False)
+        return True
+    else:
+        return False
 
 
 def upload_pdf(path: str):
