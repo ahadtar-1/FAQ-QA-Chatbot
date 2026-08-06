9bf66b2d351cddeeb40eef6e8ac054edf4beb867
yzru01XesjWQwvwzSuOqNel9BCs23r0uNjR2 <yzru01XesjWQwvwzSuOqNel9BCs23r0uNjR2>
2026-08-06 17:50:16 +0500

    port adjustment for railway deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 The module contains the application's entry point
 """
 
+import os
 import uvicorn
 from logging_config import setup_logging
 
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "routes:app",
         host="0.0.0.0",
-        port=8000,
+        port=int(os.getenv("PORT", 8000)),
         reload=True
     )
     
